bca360f43a330d9a7ef675598323614dea274f54
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-07-18 15:29:20 +0600

    navBar add done

---
+++ src/components/Home/ExtraCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExtraCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ExtraCard;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from './Banner';
+import ExtraCard from './ExtraCard';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner />
+            <ExtraCard />
+        </div>
+    );
+};
+
+export default Home;
